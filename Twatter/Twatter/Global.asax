7ad15d9d92f0758b218a42689d4fdc27e2cd4b2c
Stuart van der Lee <stuart.van.der.lee@ordina.nl>
2017-02-22 14:37:36 +0000

    Initial commit

---
+++ Twatter/Twatter/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Twatter.MvcApplication" Language="C#" %>
---
+++ Twatter/Twatter/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Twatter.Startup))]
+namespace Twatter
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
